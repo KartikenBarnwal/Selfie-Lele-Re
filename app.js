0a9f7350a3251dd223a3ebd10088e1c75ba1430b
Niranjan Kumar <kartikenkrb@gmail.com>
2020-06-01 02:52:46 +0530

    Ready to work with databas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const hbs = require('hbs')
 const path = require('path')
+const fs = require('fs')
 
 const app = express()
 
@@ -9,12 +10,26 @@ app.set('views', 'public/views')
 hbs.registerPartials('public/partials')
 
 app.use(express.static('public'))
+app.use(express.json({limit:'1mb'})) //recieved json data samajh paye hamara server 
 
 app.get('',(req,res)=>{
     res.render('index',{
         title:"Selfie Lele Re",
         designer:"Kartiken Barnwal"
     })
+})
+
+//data being sent to server side from the webpage
+app.post('/api',(req,res)=>{
+    console.log(req.body)
+    const {lat,lon} = req.body
+    // res.json({
+    //     latitude:lat,
+    //     longitude:lon
+    // })
+    const data = JSON.stringify(req.body)
+    
+    fs.appendFileSync('data.txt',data+"\n")
 })
 
 app.get('/about',(req,res)=>{
